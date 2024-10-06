1297ba17346f1fc434454a47ead795fcb9ec1b48
Sebastian <sebaszapata2022@gmail.com>
2024-10-06 12:34:08 -0500

    uupload

---
+++ server.js
@@ -0,0 +1,49 @@
+import { CohereClientV2 } from "cohere-ai";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+// Configurar CORS para permitir solo el origen específico
+const corsOptions = {
+  origin: "http://localhost:5174",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+const cohereClient = new CohereClientV2({
+  token: process.env.COHERE_API_KEY,
+});
+
+// Ruta para interactuar con la IA
+app.post("/api/coffe", async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const response = await cohereClient.chat({
+      model: "command-r-plus",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+
+    const messageText = response.message.content[0].text;
+    res.json({ message: messageText });
+  } catch (error) {
+    console.error("Error al realizar la solicitud a Cohere:", error);
+    res.status(500).json({ error: "Error al procesar la solicitud" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor backend corriendo en http://localhost:${port}`);
+});
