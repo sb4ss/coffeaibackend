700aa95c71f51d996879db5f836437f514f7bc84
Sebastian <sebaszapata2022@gmail.com>
2025-01-03 07:03:49 -0500

    bugs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 // Configurar CORS para permitir solo el origen específico
 const corsOptions = {
-  origin: "https://coffe-ai.vercel.app/",
+  origin: "https://coffe-ai.vercel.app",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
   credentials: true,
